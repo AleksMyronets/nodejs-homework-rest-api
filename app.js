6fd39e2e4bf6a7613f8a8cb98ff4dda83191192c
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-07 11:46:28 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,9 +18,12 @@ app.use(cors());
 
 app.use(express.json());
 
+app.use(express.static("public"));
+
 app.use('/api/contacts', router);
 
 app.use('/users', authRouter);
+
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' })
