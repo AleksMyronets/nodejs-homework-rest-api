f295e47a4612e812d42add8986ff08f720cebe27
AleksMyronets <mironec.kiev.1993@gmail.com>
2023-08-09 08:16:55 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.use(cors());
 
 app.use(express.json());
 
-app.use(express.static("public"));
-
 app.use('/api/contacts', router);
 
 app.use('/users', authRouter);
+
+app.use(express.static("public"));
 
 
 app.use((req, res) => {
